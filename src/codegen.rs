2110d61e8d46d8d2ebb9a369cf35a879dd4c92ea
Dong Yang <yangdong2002@stu.xjtu.edu.cn>
2021-04-27 16:28:40 +0800

    Step 3 finished

--- src/codegen.rs
+++ src/codegen.rs
@@ -22,8 +22,8 @@ pub fn write_asm(p: &IrProg, w: &mut impl Write) -> Result<()> {
 				writeln!(w, "  sw t0, 0(sp)")?;
 			}
             IrStmt::Binary(op) => {
-                writeln!(w, "  lw t1, 0(sp)")?;
-                writeln!(w, "  lw t2, 4(sp)")?;
+                writeln!(w, "  lw t1, 4(sp)")?;
+                writeln!(w, "  lw t2, 0(sp)")?;
                 writeln!(w, "  {} t1, t1, t2", match op { Add => "add", Sub => "sub", Mul => "mul", Div => "div", Mod => "rem" })?;
                 writeln!(w, "  addi sp, sp, 4")?;
                 writeln!(w, "  sw t1, 0(sp)")?;
